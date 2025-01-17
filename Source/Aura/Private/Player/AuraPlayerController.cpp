0100acebccdaca1259e4a569d19058ddad41eec4
麻婆大豆腐。 <wangshuai9595@163.com>
2025-01-17 21:48:14 +0800

    创建PlayerController

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,70 @@
+// Copyright Xile
+
+
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	// 开启网络复制
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// check检查失败会直接中止程序
+	check(AuraContext);
+
+	// 获取增强输入系统
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	// 设置输入上下文
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// 设置显示鼠标，鼠标样式为默认
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	// 输入模式
+	FInputModeGameAndUI InputModeData;
+	// 不锁定鼠标到视口
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	// 鼠标点击视口不会隐藏
+	InputModeData.SetHideCursorDuringCapture(false);
+	// 设置输入模式
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	// 偏航，也就是Y轴
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	// 获取向前向量
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	// 向右向量
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControllerPawn = GetPawn<APawn>())
+	{
+		// 分别使用向前后向右的向量
+		ControllerPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControllerPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,43 @@
+// Copyright Xile
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+	// 调用栈：
+	// UWorld被创建,Tick过程中调用到GameMode的PostSeamlessTravel
+	// PostSeamlessTravel:获取客户端中所有的PlayerController调用HandleSeamlessTravelPlayer
+	// HandleSeamlessTravelPlayer：创建PlayerController，调用SetPlayer
+	// SetPlayer:创建本地玩家，并调用InitInputSystem
+	// InitInputSystem：创建PlayerInput，调用SetupInputComponent
+	// SetupInputComponent:绑定各种输入事件
+	virtual void SetupInputComponent() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+private:
+	// 移动
+	void Move(const FInputActionValue& InputActionValue);
+};
